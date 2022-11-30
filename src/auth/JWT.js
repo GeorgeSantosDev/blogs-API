fe4131099c5972bfe64663b763db5ec00b7dfddc
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-30 17:34:23 -0300

    feat: Create JWT function for create Token

---
+++ src/auth/JWT.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET || 'suaSenhaSecreta';
+
+const jwtConfig = {
+  algorithm: 'HS256',
+  expiresIn: '1h',
+};
+
+const createToken = (userEmail) => {
+  const token = jwt.sign({ data: userEmail }, secret, jwtConfig);
+  return token;
+};
+
+module.exports = { createToken };
