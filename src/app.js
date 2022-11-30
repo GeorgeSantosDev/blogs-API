0909b8d6bcdbfde1305eb5721894a57f6f0ce0e8
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-30 17:23:50 -0300

    feat: Create login router in app file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const { loginRouter } = require('./routers/index');
 
 // ...
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/login', loginRouter);
 
 // ...
 
