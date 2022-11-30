f26529bd91f025afe63168def5efa55fed575fb3
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-30 18:48:12 -0300

    feat: Create middleware for validate displayName field

---
+++ src/middlewares/validadeDisplayName.js
@@ -0,0 +1,12 @@
+const MINIMUM_CHARACTER = 8;
+
+module.exports = async (req, res, next) => {
+  const { displayName } = req.body; 
+
+  if (!displayName || displayName.length < MINIMUM_CHARACTER) {
+    return res.status(400)
+      .json({ message: '"displayName" length must be at least 8 characters long' });
+  }
+
+  next();
+};
