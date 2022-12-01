68eca7a1a70c7494825f13d3a6c6aaadf19ec024
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-01 16:49:32 -0300

    feat: Create new get post route  (/:id)

--- src/routers/post.router.js
+++ src/routers/post.router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { postController } = require('../controllers/index');
 const { verifyToken } = require('../auth/JWT');
+const { validateIfPostExist } = require('../middlewares/index');
  
 const router = express.Router();
 
 router.get('/', verifyToken, postController.getAllPosts);
+router.get('/:id', verifyToken, validateIfPostExist, postController.getPostById);
 
 module.exports = router;
