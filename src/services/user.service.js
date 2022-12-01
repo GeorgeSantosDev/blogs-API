48ad7cae210351727245e8a87dc3905835ed0d81
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-30 22:40:41 -0300

    feat: Create user service function findUserById

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,6 @@ const { User } = require('../models');
 
 const createUser = async (...infos) => {
   const [displayName, email, password, image] = infos;
-  console.log(infos);
   const user = await User.create({ displayName, email, password, image });
 
   return user;
@@ -12,8 +11,11 @@ const getUserByEmail = async (email) => User.findOne({ where: { email } });
 
 const findAllUsers = async () => User.findAll({ attributes: { exclude: 'password' } });
 
+const findUserById = async (id) => User.findByPk(id, { attributes: { exclude: 'password' } });
+
 module.exports = {
   getUserByEmail,
   createUser,
   findAllUsers,
+  findUserById,
 };
