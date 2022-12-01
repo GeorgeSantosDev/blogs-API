bd819f83d9bf4f34821c63e5a92e8e8e7d021bb7
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-01 14:17:41 -0300

    feat: Create Category model

---
+++ src/models/Category.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'categories',
+    underscored: true,
+  });
+
+  // Category.associate = (models) => {
+  //   Category.hasMany(models.PostCategory,
+  //     { foreignKey: 'categoryId', as: 'categories' });
+  // };
+
+  return Category;
+};
