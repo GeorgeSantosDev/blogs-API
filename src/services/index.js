8888a55e5cbfb353e051db56e8d97c0375e3c075
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-01 16:34:26 -0300

    feat: Create post service function getAllPosts

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const userService = require('./user.service');
 const categoriesService = require('./categories.service');
+const postService = require('./post.service');
 
 module.exports = {
   userService,
   categoriesService,
+  postService,
 };
