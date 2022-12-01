d1bedc56dbc47829bd75f5383cdbcec051fb2e2d
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-01 16:49:04 -0300

    feat: Create post controller  function getPostById

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -10,6 +10,17 @@ const getAllPosts = async (_req, res) => {
   }
 };
 
+const getPostById = async (req, res) => {
+  try {
+    const { post } = req.body;
+
+    res.status(200).json(post);
+  } catch (err) {
+    return res.status(500).json({ message: `Internal error: ${err}` });
+  }
+};
+
 module.exports = {
   getAllPosts,
+  getPostById,
 };
