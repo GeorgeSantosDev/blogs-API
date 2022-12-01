bbc192830433a7c50a2895d571aadd0cb226e05b
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-01 18:35:14 -0300

    feat: Create categories service function getById

--- src/services/categories.service.js
+++ src/services/categories.service.js
@@ -2,8 +2,10 @@ const { Category } = require('../models');
 
 const createCategory = async (name) => Category.create({ name });
 const getAllCategories = async () => Category.findAll();
+const getById = async (id) => Category.findByPk(id);
 
 module.exports = {
   createCategory,
   getAllCategories,
+  getById,
 };
