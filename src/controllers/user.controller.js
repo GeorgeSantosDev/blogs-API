772b8c6c5fd602e63d64ffd161a4658e6971f327
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-30 22:40:59 -0300

    feat: Create user controller  function findUserById

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,12 +4,12 @@ const { userService } = require('../services/index');
 const createUser = async (req, res) => {
   try {
     const { displayName, email, password, image } = req.body;
-    
+
     const user = await userService.createUser(displayName, email, password, image);
 
     if (user) {
       const token = createToken(email);
-  
+
       res.status(201).json({ token });
     }
   } catch (err) {
@@ -17,11 +17,26 @@ const createUser = async (req, res) => {
   }
 };
 
-const findAllUsers = async (req, res) => {
+const findAllUsers = async (_req, res) => {
   try {
     const users = await userService.findAllUsers();
-    
+
     res.status(200).json(users);
+  } catch (err) {
+    return res.status(500).json({ message: `Internal error: ${err}` });
+  }
+};
+
+const findUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await userService.findUserById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User does not exist' });
+    }
+
+    res.status(200).json(user);
   } catch (err) {
     return res.status(500).json({ message: `Internal error: ${err}` });
   }
@@ -30,4 +45,5 @@ const findAllUsers = async (req, res) => {
 module.exports = {
   createUser,
   findAllUsers,
+  findUserById,
 };
