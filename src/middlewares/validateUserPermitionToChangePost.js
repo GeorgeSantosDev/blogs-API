dccd66cd80eb2544894df092a4da39902125a54e
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-01 21:29:50 -0300

    refactor: Change validate conditional

--- src/middlewares/validateUserPermitionToChangePost.js
+++ src/middlewares/validateUserPermitionToChangePost.js
@@ -1,8 +1,13 @@
-module.exports = (req, res, next) => {
+const { postService } = require('../services/index')
+
+module.exports = async (req, res, next) => {
   const { id } = req.params;
   const { id: userId } = req.user;
 
-  if (id !== userId) return res.status(401).json({ message: 'Unauthorized user' });
+  const post = await postService.getPostById(id);
+  req.post = post;
+
+  if (userId !== post.userId) return res.status(401).json({ message: 'Unauthorized user' });
 
   next();
 };
