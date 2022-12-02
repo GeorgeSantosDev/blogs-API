375ce0c76639291b7ea6114b4413ad664a3882ee
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-01 21:30:31 -0300

    feat: Create post controller function editPost

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -33,8 +33,28 @@ const createPost = async (req, res) => {
   }
 };
 
+const editPost = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const { id } = req.params;
+
+    const { edited, date } = await postService.editPost(title, content, id);
+
+    if (edited) {
+      req.post.content = content;
+      req.post.title = title;
+      req.post.updated = date;
+
+      res.status(200).json(req.post);
+    }
+  } catch (err) {
+    return res.status(500).json({ message: `Internal error: ${err}` });
+  }
+};
+
 module.exports = {
   getAllPosts,
   getPostById,
   createPost,
+  editPost,
 };
