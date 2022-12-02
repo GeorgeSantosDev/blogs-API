ca034a00563c8fe8a58c183c78eef2af769e428b
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-12-01 21:31:00 -0300

    feat: Add put router in router path (/:id)

--- src/routers/post.router.js
+++ src/routers/post.router.js
@@ -3,16 +3,25 @@ const { postController } = require('../controllers/index');
 const { verifyToken } = require('../auth/JWT');
 const { validateIfPostExist,
   validatePostFields,
-  validateCategoriesField } = require('../middlewares/index');
+  validateCategoriesField,
+  validateUserPermitionToChangePost,
+  validateChangePostFields } = require('../middlewares/index');
  
 const router = express.Router();
 
 router.get('/', verifyToken, postController.getAllPosts);
 router.get('/:id', verifyToken, validateIfPostExist, postController.getPostById);
+
 router.post('/',
   verifyToken,
   validatePostFields,
   validateCategoriesField,
   postController.createPost);
+
+router.put('/:id',
+  verifyToken,
+  validateUserPermitionToChangePost,
+  validateChangePostFields,
+  postController.editPost);
 
 module.exports = router;
